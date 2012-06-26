3b8437f2af19d1070fac9e01b0d5d6c9a64eab70
Stuart Andrews <stu.andrews@gmail.com>
2012-06-25 23:23:18 -0400

    Nest installation

--- clone-gollum-vimwiki.sh
+++ clone-gollum-vimwiki.sh
@@ -6,7 +6,8 @@ GOLLUMVIMWIKI=TRUE
 
 if ( $GOLLUMVIMWIKI ); then
     git clone https://github.com/tub78/gollum-vimwiki.git gollum-vimwiki
-    git clone https://github.com/tub78/gollum-vimwiki.wiki.git gollum-vimwiki-wiki
+    cd gollum-vimwiki
+    git clone https://github.com/tub78/gollum-vimwiki.wiki.git wiki
 fi
 
 exit;
