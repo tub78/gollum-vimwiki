a7fe5c12da00934f62c2ec508e7a14bf6baa7253
Stuart Andrews <stu.andrews@gmail.com>
2012-06-26 23:10:11 -0400

    Simpler

--- clone-gollum-vimwiki.sh
+++ clone-gollum-vimwiki.sh
@@ -6,8 +6,7 @@ GOLLUMVIMWIKI=TRUE
 
 if ( $GOLLUMVIMWIKI ); then
     git clone https://github.com/tub78/gollum-vimwiki.git gollum-vimwiki
-    cd gollum-vimwiki
-    git clone https://github.com/tub78/gollum-vimwiki.wiki.git wiki
+    git clone https://github.com/tub78/gollum-vimwiki.wiki.git gollum-vimwiki/wiki
 fi
 
 exit;
